69164994b5135c2d644f68729f7af77b2dc3e6b3
Azcobu <81782124+Azcobu@users.noreply.github.com>
2022-04-10 19:21:14 +0930

    .

--- covid-sa2.py
+++ covid-sa2.py
@@ -8,12 +8,12 @@ import seaborn as sns
 def plot(data):
     data['7 Day Average'] = data.Cases.rolling(7).mean()
     #data.plot()
-    sns.lmplot(x='Date', y='Cases', data=data, logistic=True, ci=None)
+    sns.lmplot(x='Date', y='Cases', data=data, order=4)
+    #sns.lmplot(x='Date', y='Deaths', data=data, order=4)
     plt.show()
 
 def fetch_data():
-    filetime = os.path.getmtime('daily-stats.csv')
-    if filetime < time() - 3600:
+    if not os.path.exists('daily-stats.csv') or os.path.getmtime('daily-stats.csv') < time() - 3600:
         r = requests.get('https://raw.githubusercontent.com/M3IT/COVID-19_Data/master/Data/COVID_AU_state.csv')
         if r.status_code == 200:
             with open('daily-stats.csv', 'w') as outfile:
@@ -31,11 +31,12 @@ def process_data(indata):
     filtered = [x for x in indata if targ_state in x and '2022' in x]
     for f in filtered:
         splitline = f.split(',')
-        data[splitline[0]] = int(splitline[3])
+        data[splitline[0]] = (int(splitline[3]), int(splitline[5]))
 
     series = pd.DataFrame()
     series['Date'] = list(range(len(data.keys()))) #data.keys()
-    series['Cases'] = data.values()
+    series['Cases'] = [x[0] for x in data.values()]
+    series['Deaths'] = [x[1] for x in data.values()]
     # series['Date'] = pd.to_datetime(series['Date'])
     return series
 
