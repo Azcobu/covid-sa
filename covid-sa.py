b4ebffc978ffec881ac7cfba32dc4660028ad70d
Azcobu <81782124+Azcobu@users.noreply.github.com>
2022-08-26 15:13:59 +0930

    Update to use Covidlive data
    The code originally used data sourced from covid19data.com.au, but that site now goes days at a time without updating, so switching to a more reliable source.

--- covid-sa.py
+++ covid-sa.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import requests
 import os.path
 import seaborn as sns
+from datetime import datetime, date
 from scipy.optimize import curve_fit
 
 def logistic_model(x, a, b, c):
@@ -23,50 +24,29 @@ def plot(data):
     figure.set_size_inches(16.8, 10.5)
     plt.savefig('covid-sa-current.png', bbox_inches='tight')
 
-def fetch_data():
-    print('Downloading latest updates...', end='')
-    try:
-        r = requests.get('https://raw.githubusercontent.com/M3IT/COVID-19_Data/master/Data/COVID_AU_state.csv', stream=True)
-    except Exception as err:
-        print(f'unable to retrieve file - {err}')
-    else:
-        if r.status_code == 200:
-            print('done.')
-            return r.content.decode('utf-8')
-        else:
-            print('Failed to retrieve data.')
-
-def process_data(indata):
-    data = {}
-    indata = [x.strip() for x in indata.split('\n')]
-    targ_state = 'South Australia'
-    filtered = [x for x in indata if targ_state in x]
-    filtered = filtered[700:]
-    for f in filtered:
-        splitline = f.split(',')
-        data[splitline[0]] = (int(splitline[3]), int(splitline[5]))
-
+def get_covidlive_data(startdate):
+    url = 'https://covidlive.com.au/report/daily-cases/sa'
+    df = pd.read_html(url)[1][:-1]
+    df = df[df['NEW'] != '-']
+    dates = [datetime.strptime(date, "%d %b %y").strftime('%Y-%m-%d') for date in df['DATE']][::-1]
+    cases = [int(x) for x in df['NEW']][::-1]
+    pos = 0
+    while dates[pos] < startdate:
+        pos += 1
+    return dates[pos:], cases[pos:]
+   
+def process_data():
+    dates, cases = get_covidlive_data('2021-12-24')
     series = pd.DataFrame()
-    #series['Date'] = list(range(len(data.keys()))) # data.keys()
-    series['Date'] = data.keys()
+    series['Date'] = dates
     series['Date'] = pd.to_datetime(series['Date'])
     series.set_index('Date', inplace=True)
-    series['Cases'] = [x[0] for x in data.values()]
-    #series['Deaths'] = [x[1] for x in data.values()]
-    '''
-    x = list(range(len(data.keys())))
-    y = [x[0] for x in data.values()]
-    popt, _ = curve_fit(exponential_model, x, y)
-    curve = [exponential_model(x_val, *popt) for x_val in x]
-    series['Fitted Curve'] = curve
-    '''
+    series['Cases'] = cases
     return series
 
 def main():
-    d = fetch_data()
-    if d:
-        s = process_data(d)
-        plot(s)
+    s = process_data()
+    plot(s)
 
 if __name__ == '__main__':
     main()
